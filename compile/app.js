5c9fcbc2c0616a05af889d194b8cb12a99b7d41e
Ryu <zoelabbb@gmail.com>
2023-12-02 16:02:46 +0700

    New Update

--- compile/app.js
+++ compile/app.js
@@ -15,6 +15,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const client_1 = require("@prisma/client");
 const express_1 = __importDefault(require("express"));
 const http_errors_1 = __importDefault(require("http-errors"));
+const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
 const postRoutes_1 = __importDefault(require("./routes/postRoutes"));
 const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
@@ -23,10 +24,33 @@ app.use("/post", postRoutes_1.default);
 // Route home
 app.get('/', (_req, res) => {
     const welcome = '<h1>Welcome to Express Js & Typescript Rest API with Prisma</h1>';
-    const isi = '<p>Routes : <a href="/post">/post</a></p>';
+    const isi = '<p>Routes : <a href="/feed">/feed</a> | <a href="/user">/user</a></p>';
     const htmlresponse = welcome + isi;
     res.send(htmlresponse);
 });
+const tokenKey = 'cobadulugih';
+// Route login
+app.get('/login', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { username, password } = _req.body;
+        // Validasi username dan password
+        const user = yield prisma.user.findUnique({
+            where: {
+                username: String(username)
+            }
+        });
+        if (!user || user.username !== password) {
+            return res.status(401).json({ error: 'Username or Password incorrec' });
+        }
+        // Jika valid buat token JWT
+        const token = jsonwebtoken_1.default.sign({ userId: user.id }, tokenKey, { expiresIn: '1h' });
+        res.json({ token });
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Username or Password incorrec' });
+    }
+}));
 // Get all feed, using method findMany()
 app.get("/feed", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const posts = yield prisma.post.findMany({
@@ -43,19 +67,46 @@ app.get("/feed", (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
 }));
 // Get create user
 app.post('/user', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const result = yield prisma.user.create({
-        data: Object.assign({}, _req.body)
-    });
-    res.json(result);
-}));
-app.get('/:username', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { username } = _req.params;
-    const user = yield prisma.user.findUnique({
-        where: {
-            username: String(username)
+    try {
+        // Extrack username and email from request body
+        const { username, email } = _req.body;
+        // Check if username and email already used.
+        const existUser = yield prisma.user.findUnique({
+            where: {
+                username: String(username),
+                email: String(email)
+            }
+        });
+        if (existUser) {
+            return res.status(400).json({ message: "Username or Email already used." });
         }
-    });
-    res.json(user);
+        // If unique, create new user
+        const result = yield prisma.user.create({
+            data: Object.assign({}, _req.body)
+        });
+        res.json(result);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Sorry, please fill the data correctly !!' });
+    }
+}));
+// Get user by username
+app.get('/:username', (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { username } = _req.params;
+        // Find user by username
+        const user = yield prisma.user.findUnique({
+            where: {
+                username: String(username)
+            }
+        });
+        res.json(user);
+    }
+    catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Sorry, you cannot find this user !!' });
+    }
 }));
 // Handle 404 error
 app.use((_req, _res, next) => {
---
+++ src/controller/userController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client';
+
+import jwt from 'jsonwebtoken';
+
+const prisma = new PrismaClient();
+
+const tokenKey = 'cobadulugih';
+
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { username, password } = req.body;
+        const user = await prisma.user.findUnique({
+            where: {
+                username: String(username)
+            }
+        });
+        if (!user || user.password !== password) {
+            return res.status(401).json({ error: 'Username or Password incorrec' });
+        }
+        const token = jwt.sign({ userId: user.id }, tokenKey, { expiresIn: '1h' });
+        res.json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error.' });
+    }
+}
+
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        // Extrack username and email from request body
+        const { username, email } = req.body;
+
+        // Check if username and email already used.
+        const existUser = await prisma.user.findUnique({
+            where: {
+                username: String(username),
+                email: String(email),
+            }
+        });
+
+        if (existUser) {
+            return res.status(400).json({ message: "Username or Email already used." });
+        }
+
+        // If unique, create new user
+        const result = await prisma.user.create({
+            data: {
+                ...req.body
+            }
+        });
+        res.json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Sorry, please fill the data correctly !!' });
+    }
+}
